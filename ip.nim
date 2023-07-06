535f275786e1fdc3587d75525e65ba0f1528a894
Cooperzilla <129450762+Cooperzilla@users.noreply.github.com>
2023-07-06 18:28:22 -0500

    compaction

--- ip.nim
+++ ip.nim
@@ -1,7 +1,2 @@
 import std/osproc, std/strutils, std/re
-var t = splitLines(execProcess("ipconfig"))
-
-for i in t:
-    if find(i, re"IPv4") > -1:
-        if find(t[t.find(i) - 4], re"Wi-Fi") > -1:
-            echo i.replace(" . ", "").replace("IPv4 Address", "").replace(":", "").replace("......").replace(" ", "")
+for i in (let t = splitLines(execProcess("ipconfig")); t): (if find(i, re"IPv4") > -1 and find(t[t.find(i) - 4], re"Wi-Fi") > -1: echo i.replace(" . ", "").replace("IPv4 Address", "").replace(":", "").replace("......").replace(" ", ""))
