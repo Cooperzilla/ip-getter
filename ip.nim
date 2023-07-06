8ea104eced8e08df17fcfad83d2c290ab397bb48
Cooperzilla <129450762+Cooperzilla@users.noreply.github.com>
2023-07-06 18:08:17 -0500

    major update works now

--- ip.nim
+++ ip.nim
@@ -1,2 +1,7 @@
-import std/osproc, std/strutils
-echo splitLines(execProcess("ipconfig"))[23].replace(" . ", "").replace("IPv4 Address", "").replace(":", "").replace("......").replace(" ", "")
+import std/osproc, std/strutils, std/re
+var t = splitLines(execProcess("ipconfig"))
+
+for i in t:
+    if find(i, re"IPv4") > -1:
+        if find(t[t.find(i) - 4], re"Wi-Fi") > -1:
+            echo i.replace(" . ", "").replace("IPv4 Address", "").replace(":", "").replace("......").replace(" ", "")
